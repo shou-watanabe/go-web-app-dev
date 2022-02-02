820206c0724cae860b7c0b486a71a911ac160089
shou-watanabe <watanabeshou1520@gmail.com>
2022-02-02 23:13:25 +0900

    :sparkles: add 環境変数追加

--- chapter5/go.mod
+++ chapter5/go.mod
@@ -4,6 +4,8 @@ go 1.17
 
 require (
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/matryer/go-oauth v0.0.0-20140708041717-e63dca786bb0 // indirect
 	github.com/nsqio/go-nsq v1.1.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
--- chapter5/tiwttervotes/twitter.go
+++ chapter5/tiwttervotes/twitter.go
@@ -2,10 +2,42 @@ package main
 
 import (
 	"net"
+	"os"
 	"time"
+
+	"github.com/matryer/go-oauth/oauth"
 )
 
 var conn net.Conn
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+func setupTwitterAuth() {
+	ts := struct {
+		ConsumerKey    string
+		ConsumerSecret string
+		AccessToken    string
+		AccessSecret   string
+	}{
+		ConsumerKey:    os.Getenv("API_KEY"),
+		ConsumerSecret: os.Getenv("API_KEY_SECRET"),
+		AccessToken:    os.Getenv("ACCESS_TOKEN"),
+		AccessSecret:   os.Getenv("ACCESS_TOKEN_SECRET"),
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+}
 
 func dial(netw, addr string) (net.Conn, error) {
 	if conn != nil {
